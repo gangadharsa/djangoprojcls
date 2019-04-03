7d89389e72807035192aebc55c03d90ad53f4fbe
gangadharsa <gangadharsahoo1991@gmail.com>
2019-04-03 09:30:47 +0530

    Initial commit

---
+++ classbasedviewapp/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import View
+class GetInput(View):
+    def get(self,request):
+        return render(request,'getinput.html')
+class PostInput(View):
+    def get(self,request):
+        return render(request,'postinput.html')
+class Add(View):
+    def get(self,request):
+        try:
+            a=request.GET['t1']
+            x=int(a)
+            b=request.GET['t2']
+            y=int(b)
+            z=x+y
+            return HttpResponse("<html><body bgcolor=pink><h1>Sum is:"+str(z)+"</h1></body></html>")
+        except(ValueError):
+            return HttpResponse("Invalid Input")
+    def post(self,request):
+        try:
+            a=request.POST['t1']
+            x=int(a)
+            b=request.POST['t2']
+            y=int(b)
+            z=x+y
+            return HttpResponse("<html><body bgcolor=violet><h1>Sum is:"+str(z)+"</h1></body></html>")
+        except(ValueError):
+            return HttpResponse("Invalid Input")
+
+
+
+# Create your views here.
